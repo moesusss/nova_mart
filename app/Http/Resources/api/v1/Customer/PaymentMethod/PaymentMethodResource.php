71b3e15e228c1c1c9ca683be07d4f4044fb030c0
moesusss <moesusandar@supersevenstars.com>
2022-12-20 10:29:24 +0630

    Add Payment method in user profile api

---
+++ app/Http/Resources/api/v1/Customer/PaymentMethod/PaymentMethodResource.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Resources\api\v1\Customer\PaymentMethod;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PaymentMethodResource extends JsonResource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'                        => $this->id,
+            'name'                      => $this->name,
+            'is_active'                 => $this->is_active
+        
+        ];
+    }
+    
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'status' => true,
+        ];
+    }
+}
